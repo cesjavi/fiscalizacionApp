1f5fbe3f4e58f5bf441355f780ea16081f358075
cesjavi <cesjavi@gmail.com>
2025-07-09 02:27:29 -0300

    Add trailing newlines

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,4 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-});
+  },});
--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -2,5 +2,4 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
     cy.contains('#container', 'Ready to create an app?')
-  })
-})
+  })})
