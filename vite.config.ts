47f6b6fd5caecb71ae46ee64866ddee1e28c0e2e
cesjavi <cesjavi@gmail.com>
2025-07-10 00:48:20 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig({
     react(),
     legacy()
   ],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5173',
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
