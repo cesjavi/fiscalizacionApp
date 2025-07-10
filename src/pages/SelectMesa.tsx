282bf8c1571edfc15d40c231e515bb55b0154da4
cesjavi <cesjavi@gmail.com>
2025-07-09 21:55:36 -0300

    feat: restore mesa data after login

--- src/pages/SelectMesa.tsx
+++ src/pages/SelectMesa.tsx
@@ -9,7 +9,7 @@ import {
   IonInput,
   IonButton,
 } from '@ionic/react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const SelectMesa: React.FC = () => {
@@ -17,12 +17,30 @@ const SelectMesa: React.FC = () => {
   const [seccion, setSeccion] = useState('');
   const [circuito, setCircuito] = useState('');
   const [mesa, setMesa] = useState('');
+  const [editing, setEditing] = useState(true);
+
+  useEffect(() => {
+    const savedSeccion = localStorage.getItem('seccion');
+    const savedCircuito = localStorage.getItem('circuito');
+    const savedMesa = localStorage.getItem('mesa');
+    if (savedSeccion && savedCircuito && savedMesa) {
+      setSeccion(savedSeccion);
+      setCircuito(savedCircuito);
+      setMesa(savedMesa);
+      setEditing(false);
+    }
+  }, []);
 
   const handleNext = () => {
     localStorage.setItem('seccion', seccion);
     localStorage.setItem('circuito', circuito);
     localStorage.setItem('mesa', mesa);
+    setEditing(false);
     history.push('/voters');
+  };
+
+  const handleModify = () => {
+    setEditing(true);
   };
 
   return (
@@ -39,6 +57,7 @@ const SelectMesa: React.FC = () => {
             value={seccion}
             inputmode="numeric"
             maxlength={3}
+            disabled={!editing}
             onIonChange={(e) => setSeccion(e.detail.value ?? '')}
           />
         </IonItem>
@@ -48,6 +67,7 @@ const SelectMesa: React.FC = () => {
             value={circuito}
             inputmode="numeric"
             maxlength={3}
+            disabled={!editing}
             onIonChange={(e) => setCircuito(e.detail.value ?? '')}
           />
         </IonItem>
@@ -57,10 +77,16 @@ const SelectMesa: React.FC = () => {
             value={mesa}
             inputmode="numeric"
             maxlength={4}
+            disabled={!editing}
             onIonChange={(e) => setMesa(e.detail.value ?? '')}
           />
         </IonItem>
 
+        {!editing && (
+          <IonButton expand="block" onClick={handleModify}>
+            Modificar
+          </IonButton>
+        )}
         <IonButton
           expand="block"
           onClick={handleNext}
