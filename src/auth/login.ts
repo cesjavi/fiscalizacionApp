8857808bb5c8a69bfc48b7b2fbbd7e87392b5e9d
cesjavi <cesjavi@gmail.com>
2025-08-01 21:59:50 -0300

    Remove unused Firebase login helper and clarify backend auth

--- src/auth/login.ts
+++
@@ -1,6 +0,0 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
-
-export const login = async (email: string, password: string) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
